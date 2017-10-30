228ee0eccbf0dd495d2c16f87ed7ad8ded6b7a31
Hyeon-Mook Jerry Choi <chm073@gmail.com>
2017-10-30 21:17:53 +0900

    Add 2490

---
+++ python/boj-2490.py
@@ -0,0 +1,12 @@
+for _ in range(3):
+	yut = sum(map(int, input().split()))
+	if yut == 4:
+		print('E')
+	elif yut == 3:
+		print('A')
+	elif yut == 2:
+		print('B')
+	elif yut == 1:
+		print('C')
+	else:
+		print('D')
